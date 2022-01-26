6cdaef17f3531ccb3b2d55359104316f61100a8a
Felipe Gomes de Melo <felipegmelo.42@gmail.com>
2022-01-26 16:31:05 -0300

    Refactor main()

--- src/flir_gige/flir_gige_main.cpp
+++ src/flir_gige/flir_gige_main.cpp
@@ -1,13 +1,19 @@
 #include "flir_gige/flir_gige_node.h"
 
+#include <rclcpp/rclcpp.hpp>
+
 int main(int argc, char **argv) {
   rclcpp::init(argc, argv);
 
+  std::shared_ptr<flir_gige::FlirGigeNode> main_node = std::make_shared<flir_gige::FlirGigeNode>();
   try {
-    rclcpp::spin(std::make_shared<flir_gige::FlirGigeNode>());
+
+    main_node->Init();
+
+    rclcpp::spin(main_node);
     rclcpp::shutdown();
   }
   catch (const std::exception &e) {
-    // ROS_ERROR("%s: %s", nh.getNamespace().c_str(), e.what());
+    RCLCPP_ERROR(main_node->get_logger(), e.what());
   }
 }
