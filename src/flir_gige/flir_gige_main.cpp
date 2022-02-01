60c87dffb3e69363b73797b727210efe887da02b
Felipe Gomes de Melo <felipegmelo.42@gmail.com>
2022-02-01 09:29:15 -0300

    Start acquire thread

--- src/flir_gige/flir_gige_main.cpp
+++ src/flir_gige/flir_gige_main.cpp
@@ -5,10 +5,19 @@
 int main(int argc, char **argv) {
   rclcpp::init(argc, argv);
 
+  FlirGigeDynConfig flir_config = {
+    .raw=false,
+    .nuc_action=false,
+    .nuc_mode=0,
+    .fps=20,
+  };
+
   std::shared_ptr<flir_gige::FlirGigeNode> main_node = std::make_shared<flir_gige::FlirGigeNode>();
   try {
 
     main_node->Init();
+
+    main_node->ConfigCb(flir_config, 0);
 
     rclcpp::spin(main_node);
     rclcpp::shutdown();
